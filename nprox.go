fa34b6d0b2e318d5bba5d27474e131937362d21b
Pomlon <mjaneczek1990@gmail.com>
2023-09-20 12:55:10 +0200

    initial commit, nproxy redirects based on a dead simple rule table, drops https and only allows configured urls and nothing else

---
+++ nprox.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/v2"
+)
+
+func main() {
+
+	// define origin server URL
+	//originServerURL, err := url.Parse("https://sqlite.org/")
+	//if err != nil {
+	//	log.Fatal("invalid origin server URL")
+	//}
+
+	var cnf = koanf.New(".")
+	if err := cnf.Load(file.Provider("./config.yaml"), yaml.Parser()); err != nil {
+		log.Fatalf("Error loading config.yaml: %v", err)
+	}
+
+	var endpointsMap = cnf.StringMap("endpoints")
+
+	reverseProxy := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		fmt.Printf("[reverse proxy server] received request at: %s\n", time.Now())
+
+		var path = strings.ReplaceAll(req.URL.Path, "/", "")
+		fmt.Println(path)
+
+		for key, value := range endpointsMap {
+			if key == path {
+				var ruleDest, err = url.Parse(value)
+				if err != nil {
+					fmt.Println("Error while parsing url", err)
+				}
+				fmt.Println("Found match in rule list")
+				req.Host = ruleDest.Host
+				req.URL.Host = ruleDest.Host
+				req.URL.Scheme = ruleDest.Scheme
+				req.URL.Path = ruleDest.Path
+
+			}
+		}
+
+		// set req Host, URL and Request URI to forward a request to the origin server
+		//req.Host = originServerURL.Host
+		//req.URL.Host = originServerURL.Host
+		//req.URL.Scheme = originServerURL.Scheme
+		req.RequestURI = ""
+
+		fmt.Println("Fetching ", req.URL)
+
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		// save the response from the origin server
+		originServerResponse, err := http.DefaultClient.Do(req)
+		if err != nil {
+			rw.WriteHeader(http.StatusInternalServerError)
+			_, _ = fmt.Fprint(rw, err)
+			return
+		}
+
+		// return response to the client
+
+		io.Copy(rw, originServerResponse.Body)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", reverseProxy))
+}
